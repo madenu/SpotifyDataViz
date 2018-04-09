c2a6de41fdaa75411a8cbf78595cd8737fb2d2e7
joe <joseph.jeno@gmail.com>
2018-04-08 22:29:43 -0400

    Login working!

--- spotify_data_viz/config/spotify.exs
+++ spotify_data_viz/config/spotify.exs
@@ -2,4 +2,4 @@ use Mix.Config
 
 config :spotify_ex, user_id: "1296736342",
                     scopes: ["playlist-read-private", "playlist-modify-private", "playlist-modify-public","user-top-read"],
-                    callback_url: "http://localhost:4000"
+                    callback_url: "http://localhost:4000/authenticate"
--- spotify_data_viz/lib/spotify_data_viz_web/controllers/authentication_controller.ex
+++ spotify_data_viz/lib/spotify_data_viz_web/controllers/authentication_controller.ex
@@ -4,11 +4,12 @@ defmodule SpotifyDataVizWeb.AuthenticationController do
 
   def authenticate(conn, params) do
 
-    code = %{"code" => "AQCbBWzhAMheCDS75YXcgejCMkSBWYedJenCBc9dN_tn-xtGo9eeHErzPkKN7-WJw3Yk39GywiB5bTlxCpIDXk8ssZajGlJfSyI2RcN5HarQO3InpSO80DxsJRn-bHylKUV58ETO6UujrQ7ElzE37m1sFEp3Afh3v623Ozi0TTYozYdQN_8keO0jpcsrOaA3hmDqKak5Ma0E33CPDOvyOb8JQPc44jlETND9Pwa_al148Q5aNIsDq61Efy9Qy5kWvYx5Gf9LOUg-1gtlNqcDfer2hBYQ20jjadVI1qhpmm-rRw"}
+    #delete
+    #code = %{"code" => "AQCbBWzhAMheCDS75YXcgejCMkSBWYedJenCBc9dN_tn-xtGo9eeHErzPkKN7-WJw3Yk39GywiB5bTlxCpIDXk8ssZajGlJfSyI2RcN5HarQO3InpSO80DxsJRn-bHylKUV58ETO6UujrQ7ElzE37m1sFEp3Afh3v623Ozi0TTYozYdQN_8keO0jpcsrOaA3hmDqKak5Ma0E33CPDOvyOb8JQPc44jlETND9Pwa_al148Q5aNIsDq61Efy9Qy5kWvYx5Gf9LOUg-1gtlNqcDfer2hBYQ20jjadVI1qhpmm-rRw"}
 
-    case Spotify.Authentication.authenticate(conn, code) do
+    case Spotify.Authentication.authenticate(conn, params) do
       {:ok, conn } ->
-        # do stuff
+        #do stuff
         redirect conn, to: "/profile"
       { :error, reason, conn }-> redirect conn, to: "/error"
     end
