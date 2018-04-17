f05fbc9176c2b683d72ae9ec4b33cd4e0e2ab000
William <coconuts1040@gmail.com>
2018-04-17 18:06:44 -0400

    added select box

--- spotify_data_viz/assets/js/cs/radar_spider_chart.jsx
+++ spotify_data_viz/assets/js/cs/radar_spider_chart.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import store from "./store";
+import {Form} from 'reactstrap';
+
+function Chart(props) {
+
+}
 
 function TrackAnalysis(props) {
     let token = store.getState().user_token;
 
     props.channel.join();
-    props.channel.push("recent_tracks", {token: token})
+    let recents = props.channel.push("recent_tracks", {token: token})
 
     return (<div>
+        <div className="col-3">
+            <Form>
+                <select className="form-control">
+                    {recents}
+                </select>
+            </Form>
+        </div>
+        <div>
+            <Chart />
+        </div>
     </div>);
 }
 
