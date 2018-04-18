3b70239f43eb165e46785db0dd219a47122a92ec
David Celentano <davidwcelentano@gmail.com>
2018-04-18 10:03:07 -0400

    basic spider chart added

--- spotify_data_viz/assets/js/cs/track_analysis.jsx
+++ spotify_data_viz/assets/js/cs/track_analysis.jsx
@@ -1,8 +1,55 @@
 import React from 'react';
 import {connect} from 'react-redux'
+import Radar from 'react-d3-radar';
 
 export function TrackAnalysis(props) {
-  return (<h1>TRACK ANALYSIS</h1>)
+  return (<Radar
+  width={500}
+  height={500}
+  padding={70}
+  domainMax={10}
+  highlighted={null}
+  onHover={(point) => {
+    if (point) {
+      console.log('hovered over a data point');
+    } else {
+      console.log('not over anything');
+    }
+  }}
+  data={{
+    variables: [
+      {key: 'danceability', label: 'Danceability'},
+      {key: 'energy', label: 'Energy'},
+      {key: 'tempo', label: 'Tempo'},
+      {key: 'loudness', label: 'Loudness'},
+      {key: 'speechiness', label: 'Speechiness'},
+    ],
+    sets: [
+      {
+        key: 'song1',
+        label: 'Welcome to the Jungle',
+        values: {
+          danceability: 2,
+          energy: 8,
+          tempo: 6, 
+          loudness: 8, 
+          speechiness: 7,
+        },
+      },
+      {
+        key: 'song2',
+        label: 'Levels',
+        values: {
+          danceability: 10,
+          energy: 8,
+          tempo: 8,
+          loudness: 7,
+          speechiness: 2,
+        },
+      },
+    ],
+  }}
+/>)
 }
 
 function propsFromState(state) {
